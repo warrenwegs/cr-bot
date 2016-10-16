3ce4073aa5989df525ccc7f8b14b2ffb78afb52d
Warren Wegner <warrenwegner@gmail.com>
2016-10-16 11:42:47 -0700

    adds database setup and logging of commit postings to database

--- cr-bot.js
+++ cr-bot.js
@@ -9,7 +9,7 @@ var Bot = require('slackbots');
 var CrBot = function Constructor(settings) {
     this.settings = settings;
     this.settings.name = this.settings.name || 'cr-bot';
-    this.dbPath = settings.dbPath || path.resolve(process.cwd(), 'data', 'crBot.db');
+    this.dbPath = settings.dbPath || path.resolve(process.cwd(), 'data', 'crBot.sqlite3');
 
     this.user = null;
     this.db = null;
@@ -36,11 +36,24 @@ var CrBot = function Constructor(settings) {
 
     CrBot.prototype._connectDb = function() {
         if (!fs.existsSync(this.dbPath)) {
-            console.error('Database path ' + '"' + this.dbPath + '" does not exist or it\'s not readable.');
-            process.exit(1);
+            console.log('Database path ' + '"' + this.dbPath + '" does not exist or it\'s not readable. Creating new DB...');
+            this._createDb();
         }
 
         this.db = new SQLite.Database(this.dbPath);
+    }
+
+    CrBot.prototype._createDb = function() {
+        this.db = new SQLite.Database(this.dbPath, createTables.bind(this));
+
+        // Slack gives dates in the format datetime.uniqueIntaval ie. 12345.53245 therefore we use two columns to store these as integers to make use of looking them via date ranges in epoch time
+        function createTables() {
+            console.log('Creating Tables...');
+            this.db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY ASC, uid TEXT, name TEXT, real_name TEXT UNIQUE)");
+            this.db.run("CREATE TABLE IF NOT EXISTS commits (id INTEGER PRIMARY KEY ASC, hash TEXT UNIQUE, repository TEXT, datestamp INTEGER, intervalstamp INTEGER, user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id))");
+            this.db.run("CREATE TABLE IF NOT EXISTS reviews (id INTEGER PRIMARY KEY ASC, commit_hash TEXT, datestamp INTEGER, intervalstamp INTEGER, user_id INTEGER, commented INTEGER, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(commit_hash) REFERENCES commits(hash))");
+            console.log('Success');
+        }
     }
 
     CrBot.prototype._firstRunCheck = function() {
@@ -68,37 +81,116 @@ var CrBot = function Constructor(settings) {
     };
 
     CrBot.prototype._onMessage = function(message) {
-        // console.log(message);
-        if (this._isChatMessage(message) &&
-            !this._isFromSelf(message) &&
-            // this._isChannelConversation(message) && //only listens to the channel the bot is in anyways. I think this fails since the private channel starts with 'G'
-            this._isCodePosting(message)
-        ) {
-            console.log('heard something');
-            this._replyWithInfo(message);
+        console.log(message);
+        if (!this._isFromSelf(message)) {
+            if (this._isChatMessage(message)) {
+                this._handleCodeReviewPosting(message);
+                this._handleGitHistoryPosting(message);
+            } else {
+                this._handleReactionAdded(message);
+                this._handleReactionRemoved(message);
+            }
         }
     }
-
-    CrBot.prototype._isChatMessage = function(message) {
-        return message.type === 'message' && Boolean(message.text);
-    };
-
-    CrBot.prototype._isChannelConversation = function(message) {
-        console.log('in channel', typeof message.channel === 'string' && message.channel[0] === 'C');
-        return typeof message.channel === 'string' &&
-            message.channel[0] === 'C'; // 'C' as the first char in channel id shows chat type channel
-    };
 
     CrBot.prototype._isFromSelf = function(message) {
         console.log('from self', message.user === this.user.id);
         return message.user === this.user.id;
     };
 
-    CrBot.prototype._isCodePosting = function(message) {
-        var stringCheck = 'hi';
-        console.log('is code posting', message.text.toLowerCase().indexOf(stringCheck) > -1);
-        return message.text.toLowerCase().indexOf(stringCheck) > -1;
+    CrBot.prototype._isChatMessage = function(message) {
+        return message.type === 'message' && Boolean(message.text);
+    };
+
+    CrBot.prototype._handleReactionAdded = function(message) {
+        return message.type === 'reaction_added';
     }
+
+    CrBot.prototype._handleReactionRemoved = function(message) {
+        return message.type === 'reaction_removed';
+    }
+
+    CrBot.prototype._handleGitHistoryPosting = function(message) {
+        return;
+    }
+
+    CrBot.prototype._handleCodeReviewPosting = function(message) {
+        var pattern = new RegExp('git.kiwicollection.net\/kiwicollection\/([\\w-]*)\/(?:commit|blob)\/(\\w{40})')
+        var matches = pattern.exec(message.text);
+
+        if (matches) {
+            var repo = matches[1];
+            var commit = matches[2];
+
+            if (repo && commit) {
+                var date = message.ts.split(".")
+                var datestamp = date[0];
+                var intervalstamp = date[1];
+
+                var self = this;
+                this._getUserByUId(message.user, function(user) {
+                    self._saveCommit(commit, repo, user.id, datestamp, intervalstamp);
+
+                    self._addBotReaction(message);
+                })
+            }
+        }
+
+    }
+    CrBot.prototype._saveCommit = function(commitHash, repo, userId, datestamp, intervalstamp) {
+        console.log(commitHash, repo, userId, datestamp, intervalstamp);
+        this.db.run('INSERT INTO commits(hash, repository, user_id, datestamp, intervalstamp) VALUES($hash, $repo, $user, $date, $interval)', {
+            $hash: commitHash,
+            $repo: repo,
+            $user: userId,
+            $date: datestamp,
+            $interval: intervalstamp,
+        }, function(err, record) {
+            if (err) {
+                return console.error('DATABASE ERROR:', err);
+            } 
+        });
+    }
+
+    CrBot.prototype._getUserByUId = function(uid, callback) {
+        var db = this.db;
+        var self = this;
+
+        this.db.get('SELECT * FROM users WHERE uid = ?', uid, function(err, user) {
+            if (err) {
+                return console.error('DATABASE ERROR:', err);
+            }
+
+            if (!user) {
+                var user = self._getUserById(uid);
+
+                // Move slack's id to uid property 
+                user.uid = user.id;
+                user.id = null;
+                db.run('INSERT INTO users(uid, name, real_name) VALUES(?, ?, ?)', user.uid, user.name, user.real_name, function(err) {
+                    if (err) {
+                        return console.error('DATABASE ERROR:', err);
+                    }
+                    user.id = this.lastID;
+                    callback(user);
+
+                });
+            } else {
+                callback(user);
+            }
+        });
+    }
+
+
+
+
+
+
+    CrBot.prototype._isChannelConversation = function(message) {
+        console.log('in channel', typeof message.channel === 'string' && message.channel[0] === 'C');
+        return typeof message.channel === 'string' &&
+            message.channel[0] === 'C'; // 'C' as the first char in channel id shows chat type channel
+    };
 
     CrBot.prototype._replyWithInfo = function(message) {
         var self = this;
@@ -139,6 +231,16 @@ var CrBot = function Constructor(settings) {
             return item.id === userId;
         })[0];
     };
+
+    CrBot.prototype._addBotReaction = function(message) {
+        var params = {
+            name: 'robot_face',
+            channel: message.channel,
+            timestamp: message.ts
+        };
+
+        return this._api('reactions.add', params);
+    }
 };
 
 util.inherits(CrBot, Bot);
---
+++ example_messages.md
@@ -0,0 +1,59 @@
+Warren Wegner pushed to branch develop of kiwicollection/kiwi-main (Compare changes)
+<https://git.kiwicollection.net/kiwicollection/core/commit/3fa88153e72a34f6529aea80d9ce25ffbde0cd85|db5c4778:> #37025 - fixes AvailabilityManagerTest for updated fixutres
+- warrenw 
+
+
+{ type: 'hello' }
+{ type: 'reconnect_url',
+  url: 'wss://mpmulti-oen4.slack-msgs.com/websocket/N_zC5UHTckXJkaSdrDgwjJVFqtjCOwA0HUtY5tNeMDSHeVfEray9CQDimb6ZZ-5anF1B7G9v0Ry4lubLDurRwxO2EEBdm0LNxvshGDRiOiR2krNwyI-1tRoEd5Dc_SkPLtvwuaOhtC_i3ggWPnRvv2UglgMazMCC9cuPtovInoY=' }
+{ type: 'presence_change',
+  presence: 'active',
+  user: 'U2EG9V84S' }
+{ type: 'presence_change', presence: 'away', user: 'U04MWSD7K' }
+{ type: 'presence_change',
+  presence: 'active',
+  user: 'U0DM5KLSZ' }
+{ text: 'Jamie Hall pushed to branch <https://git.kiwicollection.net/kiwicollection/core/commits/develop|develop> of <https://git.kiwicollection.net/kiwicollection/core|kiwicollection/core> (<https://git.kiwicollection.net/kiwicollection/core/compare/b0bbfd1f0d6d14a51e7af5ddfce9f37ce0452d44...4c6280e35e88544840472e5eef599d0652e0b576|Compare changes>)',
+  username: 'gitlabbot',
+  bot_id: 'B04S67ZS2',
+  attachments: 
+   [ { fallback: '<https://git.kiwicollection.net/kiwicollection/core/commit/4c6280e35e88544840472e5eef599d0652e0b576|4c6280e3>: added the geobounding and geodistance criteria to the search client and moved the error classes to the base folder\n - jamieh',
+       text: '<https://git.kiwicollection.net/kiwicollection/core/commit/4c6280e35e88544840472e5eef599d0652e0b576|4c6280e3>: added the geobounding and geodistance criteria to the search client and moved the error classes to the base folder\n - jamieh',
+       id: 1,
+       color: '334455' } ],
+  type: 'message',
+  subtype: 'bot_message',
+  team: 'T04L2KQFW',
+  user_team: 'T04L2KQFW',
+  channel: 'C04LDVBBS',
+  ts: '1475516017.000551' }
+from self false
+is code posting false
+
+{ type: 'message',
+  channel: 'G2EFS8NP8',
+  user: 'U0DM5KLSZ',
+  text: 'here\'s a posting',
+  ts: '1476570258.000004',
+  team: 'T04L2KQFW' }
+
+{ type: 'reaction_added',
+  user: 'U0DM5KLSZ',
+  item: 
+   { type: 'message',
+     channel: 'G2EFS8NP8',
+     ts: '1475515808.000004' },
+  reaction: 'beers',
+  item_user: 'U0DM5KLSZ',
+  event_ts: '1476569923.334581',
+  ts: '1476569923.000002' }
+{ type: 'reaction_removed',
+  user: 'U0DM5KLSZ',
+  item: 
+   { type: 'message',
+     channel: 'G2EFS8NP8',
+     ts: '1475515808.000004' },
+  reaction: 'beers',
+  item_user: 'U0DM5KLSZ',
+  event_ts: '1476569941.334673',
+  ts: '1476569941.000003' }
